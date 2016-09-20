823378c005cf25035b00a31670fc2c3df0b12f6c
Mohd Sufiyan <mosufy@gmail.com>
2016-09-21 01:48:53 +0800

    Search filter example

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,8 @@
 <body>
 <div id="container"></div>
 <script type="text/babel">
-  var Counter = React.createClass({
-    incrementCount: function(){
+  var MyName = React.createClass({
+    incrementCount: function () {
       this.setState({
         count: this.state.count + 1
       });
@@ -29,9 +29,63 @@
     }
   });
 
+  var FilteredList = React.createClass({
+    filterList: function (event) {
+      var updatedList = this.state.initialItems;
+      updatedList = updatedList.filter(function (item) {
+        return item.toLowerCase().search(
+            event.target.value.toLowerCase()) !== -1;
+      });
+      this.setState({items: updatedList});
+    },
+    getInitialState: function () {
+      return {
+        initialItems: [
+          "Apples",
+          "Broccoli",
+          "Chicken",
+          "Duck",
+          "Eggs",
+          "Fish",
+          "Granola",
+          "Hash Browns"
+        ],
+        items: []
+      }
+    },
+    componentWillMount: function () {
+      this.setState({items: this.state.initialItems})
+    },
+    render: function () {
+      return (
+        <div className="filter-list">
+          <input type="text" placeholder="Search" onChange={this.filterList}/>
+          <List items={this.state.items}/>
+        </div>
+      )
+    }
+  });
+
+  var List = React.createClass({
+    render: function () {
+      return (
+        <ul>
+          {
+            this.props.items.map(function (item) {
+              return <li key={item}>{item}</li>
+            })
+          }
+        </ul>
+      )
+    }
+  });
+
   ReactDOM.render(
-    <Counter name="me"/>, document.getElementById('container')
-  );
+    <div>
+      <MyName name="me"/>
+      <FilteredList/>
+    </div>,
+    document.getElementById('container'));
 </script>
 </body>
 </html>
