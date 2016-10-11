b7019df54d41e6b14e82eb283cb25719b3a71f52
Mohd Sufiyan <mosufy@gmail.com>
2016-10-11 23:51:56 +0800

    Bootstrap CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,8 @@
 
   <!-- Bootstrap -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <link href="css/jumbotron-narrow.css" rel="stylesheet">
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
