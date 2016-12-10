ccf6cef0cb2ef744bffde56e132c07a37f0c6c0c
Mohd Sufiyan <sufiyan@incube8.sg>
2016-12-10 09:09:50 +0800

    cleanup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\ElasticsearchService;
-use Elasticsearch\ClientBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
